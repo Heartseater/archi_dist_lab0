1eb0b3f5ffccee077b8aed8f0fd67b22fd78ac4e
Yahya <102614726+Heartseater@users.noreply.github.com>
2025-11-17 15:50:56 +0000

    n

--- process.c
+++ process.c
@@ -3,14 +3,12 @@
 #include <errno.h>
 #include <netinet/in.h>
 #include <pthread.h>
-#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <time.h>
 
 #define BASE_PORT 50000
 #define MAXLINE 4096
@@ -22,16 +20,19 @@ int my_pid = -1;
 int total_locks = 0; /* total number of Lock instructions in the input file (global termination) */
 
 /* Lamport clock */
-int lc = 0;
-pthread_mutex_t lc_m = PTHREAD_MUTEX_INITIALIZER;
-int inc_lc(void) {
+/* Lamport clock (logical clock) and helpers. */
+static int lc = 0;
+static pthread_mutex_t lc_m = PTHREAD_MUTEX_INITIALIZER;
+/* Increment logical clock and return new value. */
+static int inc_lc(void) {
     pthread_mutex_lock(&lc_m);
     lc++;
     int tmp = lc;
     pthread_mutex_unlock(&lc_m);
     return tmp;
 }
-int update_lc_on_receive(int remote_lc) {
+/* Update local logical clock after receiving a timestamp. */
+static int update_lc_on_receive(int remote_lc) {
     pthread_mutex_lock(&lc_m);
     if (remote_lc >= lc) lc = remote_lc + 1;
     int tmp = lc;
@@ -45,10 +46,12 @@ typedef struct ReqEntry {
     int req_pid;
     struct ReqEntry *next;
 } ReqEntry;
-ReqEntry *queue_head = NULL;
-pthread_mutex_t queue_m = PTHREAD_MUTEX_INITIALIZER;
+/* Request queue ordered by (req_lc, req_pid). */
+static ReqEntry *queue_head = NULL;
+static pthread_mutex_t queue_m = PTHREAD_MUTEX_INITIALIZER;
 
-void queue_insert(int req_lc, int req_pid) {
+/* Insert a request into the ordered queue. */
+static void queue_insert(int req_lc, int req_pid) {
     pthread_mutex_lock(&queue_m);
     ReqEntry **pp = &queue_head;
     while (*pp) {
@@ -62,7 +65,8 @@ void queue_insert(int req_lc, int req_pid) {
     pthread_mutex_unlock(&queue_m);
 }
 
-void queue_remove(int req_lc, int req_pid) {
+/* Remove a request from the queue (if present). */
+static void queue_remove(int req_lc, int req_pid) {
     pthread_mutex_lock(&queue_m);
     ReqEntry **pp = &queue_head;
     while (*pp) {
@@ -77,7 +81,8 @@ void queue_remove(int req_lc, int req_pid) {
     pthread_mutex_unlock(&queue_m);
 }
 
-int queue_head_is(int req_lc, int req_pid) {
+/* Check whether given request is at the head of the queue. */
+static int queue_head_is(int req_lc, int req_pid) {
     pthread_mutex_lock(&queue_m);
     int ok = 0;
     if (queue_head && queue_head->req_lc == req_lc && queue_head->req_pid == req_pid) ok = 1;
@@ -85,16 +90,18 @@ int queue_head_is(int req_lc, int req_pid) {
     return ok;
 }
 
-/* Track ACKs for current request: store last ack lc per peer */
-int ack_lc[MAX_PEERS];
-pthread_mutex_t ack_m = PTHREAD_MUTEX_INITIALIZER;
-void set_ack(int from, int value) {
+/* Track ACKs for current request: store last ack logical clock per peer. */
+static int ack_lc[MAX_PEERS];
+static pthread_mutex_t ack_m = PTHREAD_MUTEX_INITIALIZER;
+/* Record an ACK from peer `from`. */
+static void set_ack(int from, int value) {
     if (from < 0 || from >= MAX_PEERS) return;
     pthread_mutex_lock(&ack_m);
     ack_lc[from] = value;
     pthread_mutex_unlock(&ack_m);
 }
-int all_acks_ge(int target_lc) {
+/* Return true if all peers have ACKed at least `target_lc`. */
+static int all_acks_ge(int target_lc) {
     pthread_mutex_lock(&ack_m);
     for (int i = 0; i < N; ++i) {
         if (ack_lc[i] < target_lc) { pthread_mutex_unlock(&ack_m); return 0; }
@@ -103,16 +110,18 @@ int all_acks_ge(int target_lc) {
     return 1;
 }
 
-/* Track releases seen per process for Wait semantics */
-int releases_seen[MAX_PEERS];
-pthread_mutex_t rel_m = PTHREAD_MUTEX_INITIALIZER;
-void inc_release_seen(int pid) {
+/* Track releases seen per process for Wait semantics. */
+static int releases_seen[MAX_PEERS];
+static pthread_mutex_t rel_m = PTHREAD_MUTEX_INITIALIZER;
+/* Increment releases_seen counter for `pid`. */
+static void inc_release_seen(int pid) {
     if (pid < 0 || pid >= MAX_PEERS) return;
     pthread_mutex_lock(&rel_m);
     releases_seen[pid] += 1;
     pthread_mutex_unlock(&rel_m);
 }
-int get_release_seen(int pid) {
+/* Read releases_seen for `pid`. */
+static int get_release_seen(int pid) {
     if (pid < 0 || pid >= MAX_PEERS) return 0;
     pthread_mutex_lock(&rel_m);
     int v = releases_seen[pid];
@@ -120,8 +129,8 @@ int get_release_seen(int pid) {
     return v;
 }
 
-/* send a short-lived message to peer 'pid' */
-void send_short(int pid, const char *msg) {
+/* Send a short-lived TCP message to peer `pid` (connect, write, close). */
+static void send_short(int pid, const char *msg) {
     if (pid < 0 || pid >= N) return;
     struct sockaddr_in peeraddr;
     peeraddr.sin_family = AF_INET;
@@ -144,18 +153,19 @@ void send_short(int pid, const char *msg) {
     }
 }
 
-/* Broadcast to all other processes (short connections) */
-void broadcast_short(const char *msg) {
+/* Broadcast `msg` to all other peers using short-lived connections. */
+static void broadcast_short(const char *msg) {
     for (int i = 0; i < N; ++i) {
         if (i == my_pid) continue;
         send_short(i, msg);
     }
 }
 
-/* Incoming connection reader */
-void process_line(const char *line);
+/* Forward declaration for incoming-line parser. */
+static void process_line(const char *line);
 
-void *conn_reader(void *arg) {
+/* Reader thread for a connected socket: read lines and dispatch them. */
+static void *conn_reader(void *arg) {
     int s = *(int*)arg;
     free(arg);
     FILE *f = fdopen(s, "r");
@@ -169,8 +179,8 @@ void *conn_reader(void *arg) {
     return NULL;
 }
 
-/* Server thread: accept incoming connections */
-void *server_thread(void *arg) {
+/* Server thread: bind/listen on BASE_PORT+my_pid and accept connections. */
+static void *server_thread(void *arg) {
     (void)arg;
     int srv = socket(AF_INET, SOCK_STREAM, 0);
     int on = 1;
@@ -199,11 +209,8 @@ void *server_thread(void *arg) {
     return NULL;
 }
 
-/* Connector thread: open outgoing short-lived connection to notify peers with HELLO
-   then spawn a reader for the outgoing socket so we also accept messages that peers
-   send back on that connection (not strictly necessary for short-messaging design,
-   but we do HELLO then close; readers already exist from accept path). */
-void *connector_thread(void *arg) {
+/* Connector thread: notify peers with an initial HELLO message. */
+static void *connector_thread(void *arg) {
     (void)arg;
     for (int i = 0; i < N; ++i) {
         if (i == my_pid) continue;
@@ -228,8 +235,8 @@ void *connector_thread(void *arg) {
     return NULL;
 }
 
-/* parse and handle messages (HELLO / REQ / ACK / REL) */
-void process_line(const char *line) {
+/* Parse and handle a single incoming textual message line. */
+static void process_line(const char *line) {
     char type[16];
     int a,b,c,d;
     if (sscanf(line, "%15s %d %d %d %d", type, &a, &b, &c, &d) < 1) return;
@@ -259,8 +266,8 @@ void process_line(const char *line) {
     }
 }
 
-/* Request -> wait -> execute critical -> release */
-int do_request(int duration) {
+/* Issue a REQ for the critical section and wait for permission. */
+static int do_request(int duration) {
     int my_req_lc = inc_lc();
     queue_insert(my_req_lc, my_pid);
 
@@ -297,8 +304,8 @@ int do_request(int duration) {
     return 0;
 }
 
-/* Wait until other process has released at least once after we observed it (simple semantics) */
-void do_wait(int other_pid) {
+/* Simple wait: block until `other_pid` has produced another release. */
+static void do_wait(int other_pid) {
     int seen = get_release_seen(other_pid);
     while (1) {
         usleep(100000);
@@ -307,8 +314,8 @@ void do_wait(int other_pid) {
     }
 }
 
-/* Run instructions in input file for this process id */
-void run_instructions(const char *filename) {
+/* Execute the instruction list from `filename` for this process id. */
+static void run_instructions(const char *filename) {
     FILE *f = fopen(filename, "r");
     if (!f) { perror("open input"); exit(1); }
     char *line = NULL;
@@ -335,8 +342,8 @@ void run_instructions(const char *filename) {
     fclose(f);
 }
 
-/* Return the sum of releases_seen[0..N-1] (thread-safe) */
-int total_releases_seen(void) {
+/* Return the total number of releases observed across peers. */
+static int total_releases_seen(void) {
     int s = 0;
     pthread_mutex_lock(&rel_m);
     for (int i = 0; i < N; ++i) s += releases_seen[i];
